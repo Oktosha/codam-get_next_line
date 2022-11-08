505fa9850b3b416a48f4e422e57ecf161dbd300c
Daria Kolodzey <dkolodze@student.codam.nl>
2022-11-08 17:32:05 +0100

    Add get_next_line header

---
+++ task/get_next_line.h
@@ -0,0 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   get_next_line.h                                    :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: dkolodze <dkolodze@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2022/11/08 17:25:05 by dkolodze      #+#    #+#                 */
+/*   Updated: 2022/11/08 17:30:41 by dkolodze      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+char	*get_next_line(int fd);
+
+#endif
